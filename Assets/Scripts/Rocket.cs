07fc5b03e56ee45d94c44f1164829248c885062e
Andrew Maher <andrewaacodo12@gmail.com>
2019-10-11 18:14:17 +0200

    Input Control

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Reference to the rocket's rigidbody
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) //can thrust while totating
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotate Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotate Right");
+        }
+    }
+}
