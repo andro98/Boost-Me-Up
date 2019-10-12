9592a5776d9962cf0e717fe157d53dabc63ebc18
Andrew Maher <andrewaacodo12@gmail.com>
2019-10-12 16:47:49 +0200

    Playing With Rocket
    Improve rocket movement
    
    add sound to it and a collider

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         //Reference to the rocket's rigidbody
         rigidBody = GetComponent<Rigidbody>();
+        //Reference to the rocket's Audio Source
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -22,18 +25,38 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
-        if (Input.GetKey(KeyCode.Space)) //can thrust while totating
+        Thrust();
+        Rotate();
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space)) //Can thrust while totating
         {
             rigidBody.AddRelativeForce(Vector3.up);
+
+            if (!audioSource.isPlaying)
+                audioSource.Play();
         }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; //take manual control of rotation
 
         if (Input.GetKey(KeyCode.A))
         {
-            print("Rotate Left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Rotate Right");
+            transform.Rotate(-Vector3.forward);
         }
+
+        rigidBody.freezeRotation = false; //resume physics control of rotation
     }
 }
