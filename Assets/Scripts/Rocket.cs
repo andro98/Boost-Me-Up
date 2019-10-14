cfa4c8c63999891a3605b802b8c982fe261e9c92
Andrew Maher <andrewaacodo12@gmail.com>
2019-10-14 19:45:22 +0200

    Add multiple Audio, Refactor

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -3,11 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
-    //TODO fix lighting
     [SerializeField]
     float rcsThrust = 10f;
     [SerializeField]
     float mainThrust = 10f;
+    [SerializeField]
+    AudioClip mainEngine;
+    [SerializeField]
+    AudioClip success;
+    [SerializeField]
+    AudioClip death;
 
     Rigidbody rigidBody;
     AudioSource audioSource;
@@ -27,7 +32,7 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(state == State.Alive)
+        if (state == State.Alive)
         {
             ProcessInput();
         }
@@ -35,28 +40,42 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
-        Thrust();
-        Rotate();
+        RespondToThrustInput();
+        RespondToRotate();
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if(state != State.Alive) { return; } // ignore collisions when dead
+        if (state != State.Alive) { return; } // ignore collisions when dead
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 // do nothing
                 break;
             case "Finish":
-                state = State.Transcending;
-                Invoke("LoadNextScene", 1f); //TODO parametrise time
+                StartSuccessSquence();
                 break;
             default:
-                state = State.Dying;
-                Invoke("LoadFirstLevel", 1f);
+                StartDeathSequence();
                 break;
 
         }
+    }
+
+    private void StartSuccessSquence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        Invoke("LoadNextScene", 1f); //TODO parametrise time
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        Invoke("LoadFirstLevel", 1f);
     }
 
     private void LoadNextScene()
@@ -70,14 +89,11 @@ public class Rocket : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
-    private void Thrust()
+    private void RespondToThrustInput()
     {
         if (Input.GetKey(KeyCode.Space)) //Can thrust while totating
         {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-
-            if (!audioSource.isPlaying)
-                audioSource.Play();
+            ApplyThrust();
         }
         else
         {
@@ -85,7 +101,15 @@ public class Rocket : MonoBehaviour
         }
     }
 
-    private void Rotate()
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+
+        if (!audioSource.isPlaying)
+            audioSource.PlayOneShot(mainEngine);
+    }
+
+    private void RespondToRotate()
     {
         rigidBody.freezeRotation = true; //take manual control of rotation
 
